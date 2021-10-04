ae71c4701ea0a2aa6228bfdf594aed58f4efd5f3
s850042002 <s850042002@gmail.com>
2021-10-04 17:30:25 +0800

    adjust install-essentials.ps1

--- windows/install-essentials.ps1
+++ windows/install-essentials.ps1
@@ -1,15 +1,19 @@
-# install chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+# check whether chocolatey is already exist
+if (!(Get-Command choco.exe -errorAction SilentlyContinue)) {
+  # install chocolatey
+  Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-if (Get-Command choco.exe -errorAction SilentlyContinue) {
-  Write-Host "install chocolatey successfully"
-  # show version of chocolatey
-  choco version
+  if (Get-Command choco.exe -errorAction SilentlyContinue) {
+    Write-Host "install chocolatey successfully"
+  }
+  else {
+    Write-Host "install chocolatey failed"
+    return
+  }
 }
-else {
-  Write-Host "install chocolatey failed"
-  return
-}
+
+# show version of chocolatey
+choco outdated
 
 # install essential tools
 choco install git.install -y
