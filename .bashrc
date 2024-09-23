b9db8583fd376396c64189001319907828d26430
s850042002 <s850042002@gmail.com>
2024-09-23 11:57:44 +0800

    update linux essentials

--- .bashrc
+++ .bashrc
@@ -116,6 +116,11 @@ if ! shopt -oq posix; then
   fi
 fi
 
+# nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 # path
 PATH=$PATH:/usr/local/go/bin:/home/johnny/go/bin
 
--- install-essentials.sh
+++ install-essentials.sh
@@ -8,15 +8,26 @@ else
   apt update && apt upgrade -y
 
   apt install build-essential
-  apt install git
   apt install tmux
   apt install curl
 
-  # install golang 1.17.6
-  curl -L -o go1.17.6.linux-amd64.tar.gz https://golang.org/dl/go1.17.6.linux-amd64.tar.gz
+  # install git
+  echo "installing git..."
+  sudo add-apt-repository -y ppa:git-core/ppa
+  sudo apt-get update
+  sudo apt-get install git -y
+
+  # install golang 1.23.1
+  echo "installing golang 1.23.1..."
+  curl -L -o go1.23.1.linux-amd64.tar.gz https://golang.org/dl/go1.23.1.linux-amd64.tar.gz
   rm -rf /usr/local/go
-  tar -C /usr/local -xzf go1.17.6.linux-amd64.tar.gz
+  tar -C /usr/local -xzf go1.23.1.linux-amd64.tar.gz
+
+  # install nvm
+  echo "installing nvm..."
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 
   # install starship prompt
+  echo "installing starship prompt..."
   sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 fi
